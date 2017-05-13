a07ec111068044ee6cc7cf8ef79d1e803de7b4be
Eduardo <edcarril@ucsd.edu>
2017-05-13 15:19:02 -0700

    Generalized code so can test different input easier.

--- Peak.java
+++ Peak.java
@@ -9,12 +9,17 @@ public class Peak{
 public static void main(String[] args){
 
     System.out.println("Modular list size: 20");
-    
-    //Pick a random value for I
-    int randomNum = ThreadLocalRandom.current().nextInt(1, 19);
+   //This changes size of bimodular array 
+    int bimod_size = 20; 
+    //Range of random values
+    int random_range = 100;
+
+
     Random rand = new Random();
-    int[] increasing = new int[randomNum];
-    int[] decreasing = new int[20 - randomNum];
+    //Pick a random value for I
+    int I = ThreadLocalRandom.current().nextInt(1, bimod_size-1);
+    int[] increasing = new int[I];
+    int[] decreasing = new int[bimod_size - I];
     int[] uniqueness_inc = new int[10000];
     int[] uniqueness_dec = new int[10000];
     //Init each value to -1
@@ -24,7 +29,7 @@ public static void main(String[] args){
     }
     int counter = 0;
     while (counter < increasing.length){
-       int nextInt = rand.nextInt(100);
+       int nextInt = rand.nextInt(random_range);
        if (uniqueness_inc[nextInt] != -1){
            System.out.println(nextInt + " was already in the list!");
        }else{
@@ -37,7 +42,7 @@ public static void main(String[] args){
 
     counter = 0;
         while (counter < decreasing.length){
-        int nextInt = rand.nextInt(100);
+        int nextInt = rand.nextInt(random_range);
         if (uniqueness_dec[nextInt] != -1){
            System.out.println(nextInt + " was already in the list");
         }else{
@@ -66,9 +71,21 @@ public static void main(String[] args){
     for (int i = 0; i < bimodular.length; i++){
         System.out.print(bimodular[i] + " ");
     }
-
+    int alg_sol = findPeak(bimodular, 1, bimodular.length-1);
+    System.out.println("\nSolution to problem: \nMax value: " + bimodular[I] + "\n Index: " + I);
+    System.out.println("Algorithm solution:\n" + "Max Value: " + bimodular[alg_sol] + "\n Index: " + 0);
+    
 
 }
+    /*@param bimod: A bimodular array with constraints seen in HW sheet
+     *@param l_lim: Current lower lim of array to do search on
+     *@param u_lim: Current upper lim of array to do search on
+     *
+     * */
+     public static int findPeak(int[] bimod, int l_lim, int u_lim){
+         //Algorithm goes here
+         return 0;
+     }
     
     
 
